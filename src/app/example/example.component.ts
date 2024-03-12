9ecf4db52e74af57f7ebbbf4e9b8ae7708b757b6
William James <Williamj3795@gmail.com>
2024-03-12 10:22:11 -0400

    added configured constructor to use apiService, added getData() method

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+import { Post } from '../post';
+import { ApiService } from '../api.service';
+
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
   styleUrls: ['./example.component.css']
 })
 export class ExampleComponent {
-
+  constructor(private apiService: ApiService) {}
+  posts:Post[] = [];
+  userId:number = 10;
+  title:string = "William";
+  ngOnInit(): void {
+    this.getData();
+  }
+  getData() {
+    this.apiService.get('posts').then((response) => {
+      console.log('Data:', response.data);
+      this.userId = response.data[0].userId;
+      this.title = response.data[0].title;
+      this.posts = response.data;
+    }
+    );
+  }
 }
