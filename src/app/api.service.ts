0725b94fae04628e0e5dd309a27ac1f754f8ff9f
William James <Williamj3795@gmail.com>
2024-03-12 10:14:46 -0400

    configure Axios in the service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import axios from 'axios';
 @Injectable({
-  providedIn: 'root'
-})
+  providedIn: 'root',
+}
+)
 export class ApiService {
-
-  constructor() { }
+  private apiUrl = 'https://jsonplaceholder.typicode.com';
+  constructor() {}
+  get(endpoint: string) {
+    return axios.get(`${this.apiUrl}/${endpoint}`);
+  }
+  post(endpoint: string, data: any) {
+    return axios.post(`${this.apiUrl}/${endpoint}`, data);
+}
+// Add other HTTP methods as needed
 }
